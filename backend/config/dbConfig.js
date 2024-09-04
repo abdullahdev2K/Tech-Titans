c020b55e27ba58bf2241f193db1f12dce2d20a43
Abdullah Irfan <abdullah2k.dev@gmail.com>
2024-09-04 21:18:16 +0500

    Remove config folder from Git tracking

--- backend/config/dbConfig.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const dbConfig = () => {
-    // Connect to the database 
-    mongoose.connect(process.env.MONGODB_CLOUD).then(conn => console.log(`Database connected successfully with ${conn.connection.host}.`)).catch(err => console.log(`Database connection failed because ${err.message}`))
-};
-
-export default dbConfig;
